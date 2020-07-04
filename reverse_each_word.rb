8fa780e607bb461e42d911dc5f6a4f1606f058fa
Christopher Torres <christorres09@gmail.com>
2020-07-04 22:49:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(sentence)
-  sentence.collect.split.reverse
+sentence_array = sentence.split
+new_sentence = []
+sentence_array.each do |words|
+  reversed = words.reverse
+  new_sentence << reversed
+end
+new_sentence.join(" ")
 end
